51a252aeaef30bc13c086af75604f7c4a40b050b
Brandon Fulljames <bfullj@gmail.com>
2019-03-07 14:10:02 -0800

    Add tests for bounds checking during move

--- lib/commands/move_test.go
+++ lib/commands/move_test.go
@@ -68,3 +68,87 @@ func TestMoveWorksSimple(t *testing.T) {
 		}
 	}
 }
+
+func TestMoveErrorsGracefullyOnInvalidInput(t *testing.T) {
+	playerID := 3
+	srcX := 0
+	srcY := 0
+	destX := 1
+	destY := 2
+	g, err := loaders.PrototypeGame()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	unit := &awfdata.Unit{
+		Name:     "SampleMover",
+		Owner:    uint32(playerID),
+		Movement: 3,
+	}
+
+	sourceTile := awfdatautil.MapTileAt(g.Map, srcX, srcY)
+
+	sourceTile.Unit = unit
+
+	destinationTile := awfdatautil.MapTileAt(g.Map, destX, destY)
+
+	// Just to be safe, clear it out
+	destinationTile.Unit = nil
+
+	// Now try to do bad moves
+	cmds := []*awfdata.CmdMove{
+		&awfdata.CmdMove{
+			Origin: &awfdata.Point{
+				X: g.Map.Width,
+				Y: uint32(srcY),
+			},
+			Destination: &awfdata.Point{
+				X: uint32(destX),
+				Y: uint32(destY),
+			},
+		},
+		&awfdata.CmdMove{
+			Origin: &awfdata.Point{
+				X: uint32(srcX),
+				Y: g.Map.Height,
+			},
+			Destination: &awfdata.Point{
+				X: uint32(destX),
+				Y: uint32(destY),
+			},
+		},
+		&awfdata.CmdMove{
+			Origin: &awfdata.Point{
+				X: uint32(srcX),
+				Y: uint32(srcY),
+			},
+			Destination: &awfdata.Point{
+				X: g.Map.Width,
+				Y: uint32(destY),
+			},
+		},
+		&awfdata.CmdMove{
+			Origin: &awfdata.Point{
+				X: uint32(srcX),
+				Y: uint32(srcY),
+			},
+			Destination: &awfdata.Point{
+				X: uint32(destX),
+				Y: g.Map.Height,
+			},
+		},
+	}
+
+	for _, cmd := range cmds {
+		err = Move(cmd, playerID, g)
+
+		if err == nil {
+			t.Errorf("Expected error but didn't get one when moving from (%d, %d) to (%d, %d)",
+				cmd.Origin.X,
+				cmd.Origin.Y,
+				cmd.Destination.X,
+				cmd.Destination.Y)
+		}
+	}
+}
