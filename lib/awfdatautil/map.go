87e367a0b49d3e2d49aafff4b2fc321aacedbfe1
Brandon Fulljames <bfullj@gmail.com>
2019-03-07 15:56:20 -0800

    Naive potential move list

--- lib/awfdatautil/map.go
+++ lib/awfdatautil/map.go
@@ -14,5 +14,60 @@ func PotentialMoves(m *awfdata.Map, p *awfdata.Point) []*awfdata.Point {
 		return nil
 	}
 
-	return nil
+	hash := func(p *awfdata.Point) uint64 {
+		return uint64(p.X) + uint64(p.Y)<<32
+	}
+
+	unhash := func(h uint64) *awfdata.Point {
+		return &awfdata.Point{
+			X: uint32(h & 0xFFFF),
+			Y: uint32(h >> 32),
+		}
+	}
+
+	potentials := make(map[uint64]bool)
+
+	// Start at the unit's position, then branch out from there
+	var explore func(p *awfdata.Point, move uint32)
+	explore = func(p *awfdata.Point, move uint32) {
+		h := hash(p)
+
+		potentials[h] = true
+
+		if move == 0 {
+			return
+		}
+
+		move--
+
+		if p.Y > 0 {
+			explore(&awfdata.Point{X: p.X, Y: p.Y - 1}, move)
+		}
+
+		if p.Y < m.Height {
+			explore(&awfdata.Point{X: p.X, Y: p.Y + 1}, move)
+		}
+
+		if p.X > 0 {
+			explore(&awfdata.Point{X: p.X - 1, Y: p.Y}, move)
+		}
+
+		if p.X < m.Width {
+			explore(&awfdata.Point{X: p.X + 1, Y: p.Y}, move)
+		}
+	}
+
+	explore(p, tile.Unit.Movement)
+
+	results := make([]*awfdata.Point, len(potentials)-1)[:0]
+
+	for k := range potentials {
+		res := unhash(k)
+
+		if !(res.X == p.X && res.Y == p.Y) {
+			results = append(results, unhash(k))
+		}
+	}
+
+	return results
 }
--- lib/awfdatautil/map_test.go
+++ lib/awfdatautil/map_test.go
@@ -41,3 +41,69 @@ func TestMapTileAt(t *testing.T) {
 		}
 	}
 }
+
+func TestPotentialMovement(t *testing.T) {
+	width := 20
+	height := 20
+	totalTiles := width * height
+
+	m := &awfdata.Map{
+		Width:  uint32(width),
+		Height: uint32(height),
+		Tiles:  make([]*awfdata.Map_Tile, totalTiles),
+	}
+
+	for i := 0; i < totalTiles; i++ {
+		m.Tiles[i] = &awfdata.Map_Tile{
+			Terrain: &awfdata.Terrain{
+				Name: "Open",
+			},
+		}
+	}
+
+	center := &awfdata.Point{X: uint32(width / 2), Y: uint32(width / 2)}
+
+	origin := MapTileAt(m, center)
+	expectedLens := []int{
+		0,
+		4,
+		12,
+		24,
+		40,
+	}
+
+	for movement := 0; movement < 5; movement++ {
+		origin.Unit = &awfdata.Unit{
+			Name:     "SampleMover",
+			Movement: uint32(movement),
+		}
+
+		moves := PotentialMoves(m, center)
+
+		if len(moves) != expectedLens[movement] {
+			t.Errorf("For movement %d expected %d moves, but got %d", movement, expectedLens[movement], len(moves))
+		}
+
+		for i, move := range moves {
+			if move.X == center.X && move.Y == center.Y {
+				t.Error("Should not get a potential move where unit already is")
+			}
+
+			dist := ManhattenDistance(move, center)
+
+			if dist > uint32(movement) {
+				t.Error("Exceeded movement")
+			}
+
+			for j, other := range moves {
+				if i == j {
+					continue
+				}
+
+				if move.X == other.X && move.Y == other.Y {
+					t.Error("Should not have duplicate moves")
+				}
+			}
+		}
+	}
+}
